9c55308fb6d98265dfe4bb7e621b1a2bfd0bbbd1
nevinnoby <nevinmadappallil@gmail.com>
2025-06-09 10:48:11 +0530

    9 jun

---
+++ charCount.js
@@ -0,0 +1,9 @@
+function charCount() {
+  const str = "Guidehouse India";
+  const words = str.split(" ");
+  const str1 = words[0] + " Technology";
+  console.log("New String", str1);
+  const len = "Technology".length;
+  console.log("Number of characters in Technology:", len);
+}
+charCount();
